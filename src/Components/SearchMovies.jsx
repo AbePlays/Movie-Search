4d6bbf6be792e9003d3a4ab9dbfa1fdecae4ea80
Abe <abhi.rawat456@gmail.com>
2020-05-22 18:01:08 +0530

    Integrated TMDB API

--- src/Components/SearchMovies.jsx
+++ src/Components/SearchMovies.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 function SearchMovies() {
+  const searchMovies = async (event) => {
+    event.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=6458ca648b70d6d3d574f8e0b2ce817d&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = res.json();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie Name
       </label>
