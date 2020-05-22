03ce461d43c4e6226c0f0d215bf5747ec3ce6a24
Abe <abhi.rawat456@gmail.com>
2020-05-22 17:34:33 +0530

    Added stylesheet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(<div></div>, document.getElementById("root"));
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
