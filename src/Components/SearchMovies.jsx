849b4ad5aafad40803daa4adcca23252faaab34e
Abe <abhi.rawat456@gmail.com>
2020-05-22 18:05:03 +0530

    Added React Hooks

--- src/Components/SearchMovies.jsx
+++ src/Components/SearchMovies.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (event) => {
     event.preventDefault();
     const url = `https://api.themoviedb.org/3/search/movie?api_key=6458ca648b70d6d3d574f8e0b2ce817d&language=en-US&query=${query}&page=1&include_adult=false`;
@@ -8,6 +11,7 @@ function SearchMovies() {
     try {
       const res = await fetch(url);
       const data = res.json();
+      setMovies(data.results);
     } catch (e) {
       console.error(e);
     }
@@ -23,6 +27,9 @@ function SearchMovies() {
         name="query"
         placeholder="Enter name of movie"
         className="input"
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
       />
       <button type="submit" className="button">
         Search
